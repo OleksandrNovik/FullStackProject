f55cbb102bae89db93728601805e527c959f5c50
Novik Oleksandr <novikoleksandr9@gmail.com>
2024-02-01 17:01:44 +0200

    Created project structure
    Added dependencies, imported Entity Framework, enabled db context as service at Program.cs

---
+++ CollabProj.Infrastructure/TopicDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CollabProj.Infrastructure
+{
+    public class TopicDbContext : DbContext
+    {
+        public TopicDbContext(DbContextOptions<TopicDbContext> options) : base(options)
+        {
+            if (Database.EnsureCreated())
+            {
+                //TODO: Add db seeding 
+                Console.WriteLine("Hello world");
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //TODO: implement OnModelCreation if needed
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
