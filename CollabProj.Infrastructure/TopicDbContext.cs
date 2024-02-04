96b8b7e2bd3c6b56c2f652da896ff9a9eee60b1b
Novik Oleksandr <novikoleksandr9@gmail.com>
2024-02-04 23:35:03 +0200

    Created database seeder Now database can be filled with test data to work with requests and services. Also I changed context code so it seeds db on creation

--- CollabProj.Infrastructure/TopicDbContext.cs
+++ CollabProj.Infrastructure/TopicDbContext.cs
@@ -8,15 +8,13 @@ namespace CollabProj.Infrastructure
     {
         public TopicDbContext(DbContextOptions<TopicDbContext> options) : base(options)
         {
-            if (Database.EnsureCreated())
-            {
-                //TODO: Add db seeding 
-                Console.WriteLine("Hello world");
-            }
+            Database.EnsureCreated();
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            //TODO: implement OnModelCreation if needed
+            // If needed we seed database with test data to make it easier work with data
+            // We use extention method for that
+            modelBuilder.Seed();
             base.OnModelCreating(modelBuilder);
         }
 
@@ -40,4 +38,98 @@ namespace CollabProj.Infrastructure
         /// </summary>
         public DbSet<ContentPiece> ContentPieces { get; set; }
     }
+    public static class ModelBuilderExtensions
+    { 
+        public static void Seed(this ModelBuilder modelBuilder) 
+        {
+            var library = new TopicLibrary()
+            {
+                Author = new FakeUser(),
+                Id = 1,
+                Title = "Sample topic library",
+                Statistics = new ContentStatistics()
+                {
+                    Id = 1,  Likes = 50,  Views = 130,  CreationDate = DateTime.Now, LastViewDate = DateTime.Now,
+                }
+            };
+            var topics = new List<Topic>()
+            {
+                new Topic()
+                {
+                    Id = 1,
+                    Title = "Programming Basics",
+                    TopicLibrary = library,
+                    Statistics = new ContentStatistics()
+                    {
+                        Id = 2, Likes = 50, Views = 123, CreationDate = DateTime.Now, LastViewDate = DateTime.Now,
+                    },
+                    TopicContent = new List<ContentPiece>()
+                    {
+                        new ContentPiece()
+                        {
+                            Id = 1, InnerText = "Introduction to Programming", Type = ContentPieceType.Header
+                        },
+                        new ContentPiece()
+                        {
+                            Id = 2, InnerText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ipsum nam eligendi. Labore magnam, dolorum, quos suscipit obcaecati officiis nihil vero ea commodi ut omnis? Eum, minima? Minus, facere sed?", Type = ContentPieceType.Text
+                        },
+                        new ContentPiece()
+                        {
+                            Id = 3, InnerText = "Functions and Closures", Type = ContentPieceType.Text
+                        }
+                    }
+                },
+                new Topic()
+                {
+                    Id = 2,
+                    Title = "Advanced Programming Concepts",
+                    TopicLibrary = library,
+                    Statistics = new ContentStatistics()
+                    {
+                        Id = 3, CreationDate= DateTime.Now, LastViewDate= DateTime.Now, Likes = 32, Views = 135
+                    },
+                    TopicContent = new List<ContentPiece>()
+                    {
+                        new ContentPiece()
+                        {
+                            Id = 4, InnerText = "Object-Oriented Programming (OOP)", Type = ContentPieceType.Header
+                        },
+                        new ContentPiece()
+                        {
+                            Id = 5, InnerText = "Design Patterns", Type = ContentPieceType.Header
+                        },
+                        new ContentPiece()
+                        {
+                            Id = 6, InnerText = "console.log(\"Hello world!\")", Type = ContentPieceType.Code
+                        },
+                    }
+                } 
+            };
+
+            var statistics = new List<ContentStatistics>
+            {
+                library.Statistics
+            };
+
+            foreach (var topic in topics)
+            {
+                topic.TopicContent.ForEach(content => content.Topic = topic);
+                statistics.Add(topic.Statistics);
+            }
+            library.Topics = topics;
+
+            var contentPieces = topics[0].TopicContent;
+
+            for (int i = 1; i < topics.Count; i++) 
+            {
+                contentPieces.AddRange(topics[i].TopicContent);
+            }
+
+            modelBuilder.Entity<TopicLibrary>().HasData(library);
+            modelBuilder.Entity<Topic>().HasData(topics);
+            modelBuilder.Entity<ContentPiece>().HasData(contentPieces);
+            modelBuilder.Entity<ContentStatistics>().HasData(statistics);
+        }
+    }
+
 }
